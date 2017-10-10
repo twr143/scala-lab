090230d843b22f9a50fab8153696cd95fe48249a
Ilya Volynin <ilya.volynin@raiffeisen.ru>
2017-10-10 17:48:19 +0300

    futures, 1

---
+++ src/FutureApp.scala
@@ -0,0 +1,37 @@
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+import scala.util.control.NonFatal
+import scala.concurrent.duration._
+
+/*
+created by Ilya Volynin at 09.10.17
+*/
+object FutureApp {
+  def main(args: Array[String]): Unit = {
+    val f1 = Future {
+      1
+    }
+    val f2 = Future {
+      2
+    }
+    val fExc = Future.failed(new Exception("my new exc"))
+    val attempts = Future.sequence(Seq(f1, f2, fExc))
+    val att2 = attempts.map(_.map {
+      case i: Int => i + 1
+      case _ => -1
+    }
+    )/*.recover {
+      case NonFatal(e) ⇒ 0
+    }*/
+    att2.onComplete{
+      case Success(seq) =>println(s"success $seq")
+      case Failure(seq) =>println(s"failure $seq")
+
+    }
+    Await.ready(att2,1.seconds)
+//    Thread.sleep(1000)
+//    println(att2)
+  }
+}
--- src/Special.scala
+++ src/Special.scala
@@ -3,22 +3,27 @@ import java.time.format.DateTimeFormatter
 import java.util.UUID
 
 object Special {
-//    implicit val ZonedDateTimeWrites: Writes[ZonedDateTime] = Writes.temporalWrites[ZonedDateTime,
-//        DateTimeFormatter](DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+  //    implicit val ZonedDateTimeWrites: Writes[ZonedDateTime] = Writes.temporalWrites[ZonedDateTime,
+  //        DateTimeFormatter](DateTimeFormatter.ISO_OFFSET_DATE_TIME)
 
-    def main(args: Array[String]): Unit = {
-        /*var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX")
-        println(ZonedDateTime.now().format(formatter))
-        formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
-        println(ZonedDateTime.now().format(formatter))
-        println("123.456.789".split("\\.").last)
-      */
-//      val sourceStr = ""
-//      val regex= "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[34][0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}"
-//      println(sourceStr.matches(regex))
-//      val ui = UUID.fromString(sourceStr)
+  def main(args: Array[String]): Unit = {
+    /*var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX")
+    println(ZonedDateTime.now().format(formatter))
+    formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+    println(ZonedDateTime.now().format(formatter))
+    println("123.456.789".split("\\.").last)
+  */
+    //      val sourceStr = ""
+    //      val regex= "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[34][0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}"
+    //      println(sourceStr.matches(regex))
+    //      val ui = UUID.fromString(sourceStr)
 
-      val strs = Seq("1","2","3")
-      println(strs.mkString("','"))
+    //      val strs = Seq("1","2","3")
+    //      println(strs.mkString("','"))
+
+    Some(5).map {
+      case x if x == 4 => println(x)
+      case _ => println("default")//match error without it
     }
+  }
 }
