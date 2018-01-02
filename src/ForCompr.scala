7f922915660f3c2c40654c60e66222ff4159f0f8
twr143 <twr143@gmail.com>
2018-01-02 18:08:47 +0300

    for compr rework 1

--- src/ForCompr.scala
+++ src/ForCompr.scala
@@ -7,14 +7,22 @@ object ForCompr {
     val bob = Person("Bob", true)
     val julie = Person("Julie", false, lara, bob)
     val persons = List(lara, bob, julie)
-    val motherKid = for (p <- persons;if !p.isMale;c <- p.children)
+    val motherKid = for (p <- persons; if !p.isMale; c <- p.children)
       yield (p.name, c.name)
     println(motherKid)
     val motherKid2 = for {
       p <- persons if false
       c <- p.children
     } yield (p.name, c.name)
-    println(motherKid2)
+    //println(motherKid2)
+    val first = Option(2)
+    val f1 = for {
+      f <- first.flatMap{
+        case 1 => None
+        case x => Some(x+1)
+      }
+    } yield f
+    println(f1)
   }
 }
 case class Person(name: String, isMale: Boolean, children: Person*)
