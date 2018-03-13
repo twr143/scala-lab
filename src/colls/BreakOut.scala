2ff720cd0e583521aa155ac73215e196dae7d7f6
Ilya Volynin <ilya.volynin@raiffeisen.ru>
2018-03-13 17:38:08 +0300

    breakout ordering, 1

--- src/colls/BreakOut.scala
+++ src/colls/BreakOut.scala
@@ -1,8 +1,14 @@
 package colls
+
 import scala.collection.breakOut
+import scala.collection.immutable.TreeMap
+
 object BreakOut {
   def main(args: Array[String]): Unit = {
-    val map = List("London", "Paris").map(x => (x.length, x))(breakOut[List[String],(Int,String),Map[Int, String]])
+    implicit object orderingForTreemap extends Ordering[Int] {
+      def compare(o1: Int, o2: Int): Int = o2 - o1
+    }
+    val map = List("London", "Paris").map(x => (x.length, x))(breakOut[List[String], (Int, String), TreeMap[Int, String]])
     println(map)
     val map2 = List("London", "Paris").map(x => (x.length, x))
     println(map2)
