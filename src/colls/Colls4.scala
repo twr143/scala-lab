a3ed8e2e840ee47be02147718c015e6c4ce1514e
twr143 <twr143@gmail.com>
2018-07-05 10:01:46 +0300

    list api, 2

--- src/colls/Colls4.scala
+++ src/colls/Colls4.scala
@@ -8,8 +8,12 @@ object Colls4 extends App {
   l1 :+= 3
   var l2 = l1.tail
   var l3 = l1.dropRight(1)
-
+  var l4 = l1.drop(2)
+  var l5 = l1.take(2)
   println(s"$l1")
   println(s"$l2")
   println(s"$l3")
+  println(s"h:${l1.head} t:${l1.tail} l:${l1.last}")
+  println(s"l4:$l4")
+  println(s"l5:$l5")
 }
